8312db45c0f90460ee87e64768f02afcd94270f8
Holger Pirk <holger@pirk.name>
2018-11-26 12:29:05 +0000

    added some comments

--- imap.hpp
+++ imap.hpp
@@ -9,8 +9,17 @@ namespace IMAP {
 class Message {
 public:
 	Message(){}
+	/**
+	 * Get the body of the message. You may chose to either include the headers or not.
+	 */
 	std::string getBody();
+	/**
+	 * Get one of the descriptor fields (subject, from, ...)
+	 */
 	std::string getField(std::string fieldname);
+	/**
+	 * Remove this mail from its mailbox
+	 */
 	void deleteFromMailbox();
 };
 
@@ -18,12 +27,26 @@ class Session {
 public:
 	Session(std::function<void()> updateUI);
 
+	/**
+	 * Get all messages in the INBOX mailbox terminated by a nullptr (like we did in class)
+	 */
 	Message** getMessages();
 
+	/**
+	 * connect to the specified server (143 is the standard unencrypte imap port)
+	 */
 	void connect(std::string const& server, size_t port = 143);
 
+	/**
+	 * log in to the server (connect first, then log in)
+	 */
 	void login(std::string const& userid, std::string const& password);
 
+	/**
+	 * select a mailbox (only one can be selected at any given time)
+	 * 
+	 * this can only be performed after login
+	 */
 	void selectMailbox(std::string const& mailbox);
 
 	~Session();
